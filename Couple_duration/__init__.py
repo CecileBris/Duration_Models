b3cd80e49000fb2d665d99b4f4a3c68c2bc56c59
Cécile Brissard <cecilebrissard12@gmail.com>
2022-11-28 16:12:17 +0100

    Fix: import

---
+++ Couple_duration/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 28 16:06:19 2022
+
+@author: ceecy
+"""
+
---
+++ Couple_duration/nivetude_function.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 28 16:10:08 2022
+
+@author: ceecy
+"""
+
+
+def nivetude(x):
+    if x == 00 :
+        return 0
+    elif x == 20 :
+        return 1
+    elif x == 30 :
+        return 2
+    elif x == 40 :
+        return 3
+    elif x == 50 :
+        return 4
